592885fcb8766df3dafe47f885916a33560225e6
ArsenEuroDed <145552121+ArsenEuroDed@users.noreply.github.com>
2024-10-08 13:27:37 +0300

    Add files via upload

---
+++ Zadanie_10/Zadanie_10/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Zadanie_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Nullable<float> number = null;
+
+
+            Console.Write(">>>Введите коэффициент масштабирования (целое число): ");
+            int s = int.Parse(Console.ReadLine());
+
+
+            Console.Write(">>>Введите смещение (целое число): ");
+            int t = int.Parse(Console.ReadLine());
+
+
+            _cScale(ref number, s, t);
+
+
+            Console.WriteLine("<<<Результат: " + number);
+        }
+
+        static void _cScale(ref Nullable<float> n, int scale, int shift)
+        {
+
+            Random rand = new Random();
+
+            n = (float)rand.NextDouble();
+
+            //масштабирование числа (умножение на scale)
+            n = n * scale;
+
+            //добавление смещения shift
+            n = n + shift;
+
+        }
+
+    }
+}
